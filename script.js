83a68cbb6abe0ba5edad52976b38f8a6ad45dafd
Telian A. Diggs <123404470+Telianakai@users.noreply.github.com>
2023-10-02 14:26:38 -0400

    Update script.js (Update for mobile menu)

--- script.js
+++ script.js
@@ -23,3 +23,36 @@ mainButton.forEach(link => {
         link.classList.remove("main-button-grow");
     });
 });
+
+// ________________Mobile Dropdown Menu_________________
+// Get the mobile menu button and mobile navigation
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const mobileNav = document.getElementById("mobileNav");
+
+// Variable to track whether the mobile navigation is open
+let isMobileNavOpen = false;
+
+// Function to toggle the mobile navigation menu
+function toggleMobileNav() {
+    if (isMobileNavOpen) {
+        mobileNav.style.display = "none";
+    } else {
+        mobileNav.style.display = "block";
+    }
+    isMobileNavOpen = !isMobileNavOpen;
+}
+
+// Show mobile navigation on button click
+mobileMenuBtn.addEventListener("click", toggleMobileNav);
+
+// Keep mobile navigation open when the mouse is inside
+mobileNav.addEventListener("mouseenter", () => {
+    mobileNav.style.display = "block";
+    isMobileNavOpen = true;
+});
+
+// Hide mobile navigation when the mouse leaves the menu
+mobileNav.addEventListener("mouseleave", () => {
+    mobileNav.style.display = "none";
+    isMobileNavOpen = false;
+});
